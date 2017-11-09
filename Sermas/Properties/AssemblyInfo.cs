0b34fc1c55412474355a571d5cff9a59fc877d98
DevutMX <devutmx@gmail.com>
2017-11-09 02:54:09 -0600

    Version del proyecto en asp

---
+++ Sermas/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sermas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sermas")]
+[assembly: AssemblyCopyright("Copyright ? 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("05c2b825-1649-493c-8f78-d8e7caf84b84")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Sermas/Vistas/index.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/Vistas/FrontEnd.Master" CodeBehind="index.aspx.cs" Inherits="Sermas.Vistas.index" %>
+
+<asp:Content ID="Content" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>hola mundo</h1>
+</asp:Content>
